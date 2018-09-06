9a198daebe0a23a0e3b0930403465111d90917d0
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-09-05 21:52:17 -0600

    feat: se obtienen correctamente los nombres de las tablas

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var queries = require("./queries");
 
 router.get("/getSchemas", queries.getSchemas);
+router.get("/getTableNames", queries.getTableNames);
 
 module.exports = router;
--- server/routes/queries.js
+++ server/routes/queries.js
@@ -15,6 +15,17 @@ async function getSchemas(req, res, next) {
   }
 }
 
+async function getTableNames(req, res, next) {
+    try {
+      let pool = await sql.connect(config);
+      let result = await pool.request().query("SELECT name FROM sys.Tables");
+      res.status(200).json({resultado: result});
+    } catch (err) {
+      console.log(err);
+    }
+}
+
 module.exports = {
-  getSchemas: getSchemas
+  getSchemas: getSchemas,
+  getTableNames: getTableNames
 };
