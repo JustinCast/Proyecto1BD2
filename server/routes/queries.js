698089576ed396c7aa66a569386a0ac4cd5e43fa
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-09-13 01:49:32 -0600

    fix: filtrado correcto de schemas

--- server/routes/queries.js
+++ server/routes/queries.js
@@ -9,7 +9,7 @@ sql.on("err", err => {
 async function getSchemas(req, res, next) {
   try {
     let pool = await sql.connect(config);
-    let result = await pool.request().query("SELECT name FROM sys.schemas");
+    let result = await pool.request().query("SELECT TABLE_SCHEMA FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA != 'sys' GROUP BY TABLE_SCHEMA");
     res.status(200).json(result.recordset);
     sql.close();
   } catch (err) {
